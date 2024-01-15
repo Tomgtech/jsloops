717b493be75ab3ac3f6f5c2db9690a4e13f9997a
Tomgtech <tom.gendall@yahoo.com>
2024-01-15 15:55:14 +0000

    scared to crash my laptop trying while loops

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("Hello World!");
+
+for (let i = 0; i < 5; i++) {
+  console.log("Iteration", i);
+}
+
+let counter = 1;
+
+let videoGames = ["skate", "gta", "elden ring"];
+
+for (let i = 0; i < 3; i++) {
+  console.log(videoGames[i]);
+}
